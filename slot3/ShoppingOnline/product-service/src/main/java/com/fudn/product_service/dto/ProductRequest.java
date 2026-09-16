5ec72b9c42c450752524458cec867d5df71c7fdf
Le Quang Vinh <lequangvinh1324902@gmail.com>
2026-09-16 14:32:52 +0700

    feat: add product DTOs

---
+++ slot3/ShoppingOnline/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
@@ -0,0 +1,10 @@
+package com.fudn.product_service.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(
+        String name,
+        String description,
+        BigDecimal price
+) {
+}
---
+++ slot3/ShoppingOnline/product-service/src/main/java/com/fudn/product_service/dto/ProductResponse.java
@@ -0,0 +1,11 @@
+package com.fudn.product_service.dto;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
+}
