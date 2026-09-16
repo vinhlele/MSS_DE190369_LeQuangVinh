7d34ac4a1e67741a5d041e53ea47ec6e448b48a2
Le Quang Vinh <lequangvinh1324902@gmail.com>
2026-09-16 14:43:01 +0700

    feat: add product controller, service

---
+++ slot3/ShoppingOnline/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
@@ -0,0 +1,53 @@
+package com.fudn.product_service.service;
+
+import com.fudn.product_service.dto.ProductRequest;
+import com.fudn.product_service.dto.ProductResponse;
+import com.fudn.product_service.model.Product;
+import com.fudn.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product {} saved", product.getId());
+
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice()
+        );
+    }
+
+    public List<ProductResponse> getAllProducts() {
+
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice()
+                ))
+                .toList();
+    }
+}
